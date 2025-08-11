d14e651b237e1aa2c0490ebcf026a83a43ab2549
aswinsbabu <aswinsbabu007@gmail.com>
2025-08-11 10:57:53 +0530

    Delete XRP-20240529T061351Z-001/asb/LLMcodeGenTest/assorted/installation_verification.py

--- XRP-20240529T061351Z-001/asb/LLMcodeGenTest/assorted/installation_verification.py
+++
@@ -1,5 +0,0 @@
-from XRPLib.defaults import *
-
-# available variables from defaults: left_motor, right_motor, drivetrain,
-#      imu, rangefinder, reflectance, servo_one, board, webserver
-# Write your code Here
