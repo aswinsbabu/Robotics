330528d5c5aaaaa0c5654f38c5d619833a2b7473
aswinsbabu <aswinsbabu007@gmail.com>
2025-08-13 15:11:04 +0530

    Create SimpleWifiDataTrnfr.py

---
+++ XRP-20240529T061351Z-001/asb/RPiXRPconnect/Basic/SimpleWifiDataTrnfr.py
@@ -0,0 +1,37 @@
+#Simple Wifi Data transfer(helloworld)
+from XRPLib.defaults import *
+
+import network
+import socket
+import time
+
+SSID = 'Narishimas'
+PASSWORD = '123456798'
+
+# Connect to Wi-Fi
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(SSID, PASSWORD)
+print("Connecting to Wi-Fi...", end="")
+while not wlan.isconnected():
+    print(".", end="")
+    time.sleep(1)
+print("\nConnected. IP:", wlan.ifconfig()[0])
+
+# Create TCP socket server
+addr = socket.getaddrinfo('0.0.0.0', 12345)[0][-1]
+s = socket.socket()
+s.bind(addr)
+s.listen(1)
+print("Listening on", addr)
+
+while True:
+    cl, client_addr = s.accept()
+    print("Client connected from", client_addr)
+    data = cl.recv(1024)
+    if data:
+        msg = data.decode().strip()
+        print("Received:", msg)
+        # Optionally reply:
+        cl.send(b"ack\n")
+    cl.close()
